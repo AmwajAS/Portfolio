9874b585c76e77e37077b54ad5f2ccace90ac957
Amwaj Abu Salah <99044391+AmwajAS@users.noreply.github.com>
2024-10-08 02:03:15 +0300

    fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,9 +38,9 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="https://amwajas.github.io/Portfolio/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
-        </Navbar.Brand>
+       <Navbar.Brand href="/Portfolio/#/" className="d-flex">
+       <img src={logo} className="img-fluid logo" alt="brand" />
+      </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           onClick={() => {
